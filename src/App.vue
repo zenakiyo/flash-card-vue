e5471231277669378589463fee9079758340b245
zenakiyo <zenakiyo@gmail.com>
2024-11-12 00:08:17 +0530

    add files

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup>
+import qandaCard from './components/qandaCard.vue';
+const data = [
+  {
+    id: 3457,
+    question: 'What language is React based on?',
+    answer: 'JavaScript',
+  },
+  {
+    id: 7336,
+    question: 'What are the building blocks of React apps?',
+    answer: 'Components',
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: 'JSX',
+  },
+  {
+    id: 1297,
+    question: 'How to pass data from parent to child components?',
+    answer: 'Props',
+  },
+  {
+    id: 9103,
+    question: 'How to give components memory?',
+    answer: 'useState hook',
+  },
+  {
+    id: 2002,
+    question: 'What do we call an input element that is completely synchronised with state?',
+    answer: 'Controlled element',
+  },
+]
+</script>
+
+<template>
+  <div class="container">
+    <qandaCard v-for="i in data" :key="i.id" :data = "i"/>
+  </div>
+</template>
+
+<style scoped>
+
+.container {
+  width: 100%;
+  height: 95vh;
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  justify-content: space-evenly;
+  align-items: center;
+  gap: auto;
+}
+</style>
---
+++ src/components/qandaCard.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+const qmode = ref(true)
+defineProps({data: Object})
+</script>
+<template>
+  <div :class="qmode ? 'divq' : 'divq diva'" @click="qmode = !qmode">
+    {{ qmode ? data.question : data.answer }}
+  </div>
+</template>
